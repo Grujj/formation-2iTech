4bbcc1aab8fa7193da8af20e773648f41799ef1f
Julien Grenier <grenier.julien5@gmail.com>
2021-10-05 10:11:37 +0200

    Organisation exercice

--- Exo/src/fr/Run.java
+++ Exo/src/fr/Run.java
@@ -1,10 +1,16 @@
 package fr;
 
+import fr.banque.Client;
+import fr.banque.Compte;
 import fr.dessin.Point2D;
 import fr.dessin.Point3D;
 
 public class Run {
     public static void main(String[] args){
+        Run.javaExo6();
+    }
+
+    public static void javaExo1a5(){
         Point2D point2D = new Point2D(4, 5);
         Point3D point3D = new Point3D(5, 6, 10);
 
@@ -20,5 +26,17 @@ public class Run {
 
         System.out.println("Compteur : ");
         Point2D.getCompteur();
+    }
+
+    public static void javaExo6(){
+
+        Client client = new Client();
+        client.ajouterCompte(new Compte());
+        client.ajouterCompte(null);
+        client.ajouterCompte(new Compte(123, 500D));
+        client.ajouterCompte(new Compte());
+        client.ajouterCompte(new Compte(345, 750D));
+
+        System.out.println(client.toString());
     }
 }
